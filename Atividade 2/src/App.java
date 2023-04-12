c23d1546b978de92041fd52df19b21e636e1a82f
LuanEvander <luanevander.dev@gmail.com>
2023-04-12 18:59:34 -0300

    Adição Atividade 5

--- Atividade 2/src/App.java
+++ Atividade 2/src/App.java
@@ -1,19 +1,22 @@
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) {
-        Aluno a1 = new Aluno("Luiz", 20);
-        Aluno a2 = new Aluno("Maria", 19);
-        Aluno a3 = new Aluno("João", 18);
-
-        VetorAluno turma = new VetorAluno();
-
-        turma.adiciona(a1);
-        turma.adiciona(a2);
-        turma.adiciona(a3);
-
-        System.out.println(turma.contem(a1));
-
-        System.out.println(turma.contem(a2));
-
-        turma.tamanho();
+    public static void main(String[] args) throws Exception {
+        // Cria um objeto PontoCartesiano com as coordenadas X(2,3) e Y(5,7)
+        PontoCartesiano ponto = new PontoCartesiano(2, 5, 3, 8); 
+        
+        // Cria um objeto PlanoCartesiano com o objeto PontoCartesiano
+        PlanoCartesiano plano = new PlanoCartesiano(ponto, 30); 
+        
+        // Cria um objeto JFrame com o título "Plano Cartesiano"
+        JFrame frame = new JFrame("Plano Cartesiano"); 
+        
+        // Define a ação padrão de fechar o JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        
+        frame.getContentPane().add(plano); // Adiciona o objeto PlanoCartesiano ao JFrame
+        frame.pack(); // Ajusta o tamanho do JFrame
+        frame.setLocationRelativeTo(null); // Centraliza o JFrame na tela
+        frame.setVisible(true); // Exibe o JFrame
     }
 }
